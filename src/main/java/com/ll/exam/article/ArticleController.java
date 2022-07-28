38028eb7e3cce1725400a6b617aa116374d31ff2
zzerowin <alaykr123@gmail.com>
2022-07-28 15:32:41 +0900

    json 응답에 무조건 resultCode와 msg가 있어야 한다는 것을 강제하게 위해서 ResultData 클래스 생성

--- src/main/java/com/ll/exam/article/ArticleController.java
+++ src/main/java/com/ll/exam/article/ArticleController.java
@@ -1,5 +1,6 @@
 package com.ll.exam.article;
 
+import com.ll.exam.ResultData;
 import com.ll.exam.Rq;
 import com.ll.exam.article.dto.ArticleDto;
 import com.ll.exam.util.Ut;
@@ -133,11 +134,7 @@ public class ArticleController {
     public void getArticles(Rq rq) {
         List<ArticleDto> articleDtos = articleService.findAll();
 
-        Map<String, Object> resultData = new LinkedHashMap<String, Object>();
-
-        resultData.put("resultCode", "S-1");
-        resultData.put("message", "성공");
-        resultData.put("data", articleDtos);
+        ResultData<List<ArticleDto>> resultData = new ResultData("성공", "S-1", articleDtos);
 
         rq.json(resultData);
     }
